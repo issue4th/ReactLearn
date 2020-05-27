a3cbce8309f8bbc4c0101b4438d55b37733b7250
Robert Pym <rob_pym@hotmail.com>
2020-05-27 12:25:12 +0100

    Templated dynamic content example with {}

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
         <h1>Hi', I'm a React app!</h1>
         <p>This is really working!</p>
         <Person />
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
   };
--- client/src/Person/Person.js
+++ client/src/Person/Person.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Person.css';
 import { render } from 'react-dom';
 
-const person = () => <p>I'm a Person!</p>;
+const person = () => (
+  <p>I'm a Person and I'm {Math.floor(Math.random() * 50)} years old!</p>
+);
 
 export default person;
