b5580b9be808320c942da1cd91ca6951a1b025a5
Robert Pym <rob_pym@hotmail.com>
2020-05-27 12:06:47 +0100

    Compose App using React.createElement

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render = () => (
-    <div className='App'>
-      <h1>Hi, I'm an empty React app</h1>
-    </div>
-  );
+  render = () => {
+    let h1 = React.createElement('h1', null, "Hi I'm a React app!");
+    return React.createElement('div', { className: 'App' }, h1);
+  };
 }
 
 export default App;
