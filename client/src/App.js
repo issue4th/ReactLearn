f183458280a549d2c07c00049449753e007794c0
Robert Pym <rob_pym@hotmail.com>
2020-05-27 12:19:18 +0100

    Create and import Person component (functional style)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render = () => {
-    let h1 = React.createElement('h1', null, "Hi I'm a React app!");
-    return React.createElement('div', { className: 'App' }, h1);
+    return (
+      <div className='App'>
+        <h1>Hi', I'm a React app!</h1>
+        <p>This is really working!</p>
+        <Person />
+      </div>
+    );
   };
 }
 
---
+++ client/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Person.css';
+import { render } from 'react-dom';
+
+const person = () => <p>I'm a Person!</p>;
+
+export default person;
