0c8c3e2ab3cb19fb2d7bb3463df567fc9a9a9dbf
Robert Pym <rob_pym@hotmail.com>
2020-05-27 12:02:32 +0100

    Use arrow function for render

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <h1>Hi, I'm a React app</h1>
-      </div>
-    );
-  }
+  render = () => (
+    <div className='App'>
+      <h1>Hi, I'm an empty React app</h1>
+    </div>
+  );
 }
 
 export default App;
