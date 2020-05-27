cdcf10d043a28e702cc648bef32b9781bc1a5025
Robert Pym <rob_pym@hotmail.com>
2020-05-27 12:34:00 +0100

    Add <Person> content with props.children

--- client/src/Person/Person.js
+++ client/src/Person/Person.js
@@ -3,9 +3,12 @@ import './Person.css';
 
 const person = (props) => {
   return (
-    <p>
-      I'm {props.name} and I'm {props.age} years old!
-    </p>
+    <div>
+      <p>
+        I'm {props.name} and I'm {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
   );
 };
 
