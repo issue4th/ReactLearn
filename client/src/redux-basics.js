11dc31476e588d73a006e5601132bdb6dd52da76
Robert Pym <rob_pym@hotmail.com>
2020-05-28 17:53:15 +0100

    Add a couple of actions

--- client/src/redux-basics.js
+++ client/src/redux-basics.js
@@ -6,15 +6,23 @@ const initialState = {
 };
 
 // Reducer
-const rootReducer = (state = initialState, action) => state;
+const rootReducer = (state = initialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return { ...state, counter: state.counter + 1 };
+  }
+
+  if (action.type === 'ADD_COUNTER') {
+    return { ...state, counter: state.counter + action.value };
+  }
+
+  return state;
+};
 
 // Store
 const store = createStore(rootReducer);
 
 // Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 
-// Subscription
-
-console.dir(rootReducer());
-console.dir(initialState);
-console.dir(store);
+console.log(store.getState());
