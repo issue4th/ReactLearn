5f5315d497a3a70db7030035fb7079f4d8ed78e5
Robert Pym <rob_pym@hotmail.com>
2020-05-29 10:23:14 +0100

    Actions identifiers as enum-like object

--- client/src/redux-basics.js
+++ client/src/redux-basics.js
@@ -1,17 +1,22 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 
+const actions = {
+  INC_COUNTER: 'INC_COUNTER',
+  ADD_COUNTER: 'ADD_COUNTER',
+};
+
 const initialState = {
   counter: 0,
 };
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
-  if (action.type === 'INC_COUNTER') {
+  if (action.type === actions.INC_COUNTER) {
     return { ...state, counter: state.counter + 1 };
   }
 
-  if (action.type === 'ADD_COUNTER') {
+  if (action.type === actions.ADD_COUNTER) {
     return { ...state, counter: state.counter + action.value };
   }
 
@@ -25,7 +30,7 @@ const store = createStore(rootReducer);
 store.subscribe(() => console.log('Subscription:', store.getState()));
 
 // Action
-store.dispatch({ type: 'INC_COUNTER' });
-store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+store.dispatch({ type: actions.INC_COUNTER });
+store.dispatch({ type: actions.ADD_COUNTER, value: 10 });
 
 console.log(store.getState());
